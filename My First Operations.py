c9452f9d4f6f9ab79e4c50f14e2abe7cdf15f39a
Evert Olsthoorn <30754@ma-web.nl>
2020-10-23 11:31:10 +0200

    Ik heb 'My First Operations' af

---
+++ My First Operations.py
@@ -0,0 +1,44 @@
+
+hoi1 = 1
+
+print (2 + 2)
+
+print (1 - 1)
+
+print (5 / 2)
+
+print (5%2)
+
+getal = 13
+
+print (getal)
+
+getal += 1
+
+print (getal)
+
+getal2 = 8
+
+print (getal2)
+
+getal2 /= 8
+
+print (getal2)
+
+print (10*10)
+
+getal3 = 4
+
+print (getal3)
+
+getal3 *= 7
+
+print (getal3)
+
+getal4 = 786
+
+print (getal4)
+
+getal4 -= 125
+
+print (getal4)
